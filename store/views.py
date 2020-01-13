ac68b27d029dd7cc586494ff177c9a1f5e25d1a7
Worek Dawid <dawid.worek@aalto.fi>
2020-01-13 13:15:46 +0200

    Working on backend

--- store/views.py
+++ store/views.py
@@ -1,10 +1,44 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-from django.views.decorators.http import require_GET, require_POST
+from django.views.decorators.http import require_GET, require_POST, require_http_methods
 from django.contrib.auth.decorators import login_required
 
-@require_GET
+@require_http_methods(["GET", "POST"])
 #@login_required(login_url='/accounts/login/') # TODO to uncomment when user will be created
 def edit_profile(request):
-    ctx = {}
-    return render(request, 'edit_profile.html', context=ctx)
+    if request.method == 'GET':
+        ctx = { # TODO get logged user: current_user = request.user
+            "current_username": "current-username-here",
+            "is_developer": True,
+            "current_payment_info": "vjreinvuierji834=",
+            "updated": False,
+        }
+
+        has_been_updated_successfully = request.GET.get('updated')
+        if has_been_updated_successfully:
+            ctx["updated"] = True
+            ctx["has_been_updated_successfully"] = has_been_updated_successfully
+            
+        return render(request, 'edit_profile.html', context=ctx)
+    elif request.method == 'POST':
+        usr_name = request.POST.get('usrName')
+        if usr_name:
+            payment_info = request.POST.get('paymentInfo')
+            if payment_info:
+                # TODO Update developer
+                # current_user.username = usr_name
+                # current_user.payment_info = payment_info
+                pass
+            else:
+                # TODO Update player
+                # current_user.username = usr_name
+                pass
+            # TODO Commit update
+            try:
+                #current_user.save()
+                return HttpResponseRedirect(".?updated=true")
+            except:
+                return HttpResponseRedirect(".?updated=false")
+        else:
+            pass # Not valid request, do nothing
+
