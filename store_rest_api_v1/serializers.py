83c79e90d7084d2af04560abde3b624614045dd7
Worek Dawid <dawid.worek@aalto.fi>
2020-02-05 17:37:04 +0200

    Optional excluding game fields from response

--- store_rest_api_v1/serializers.py
+++ store_rest_api_v1/serializers.py
@@ -6,14 +6,28 @@ class GeneralUserSerializer(serializers.HyperlinkedModelSerializer):
         model = GeneralUser
         fields = ['pk', 'username', 'email']
 
-class GameSerializer(serializers.HyperlinkedModelSerializer):
+class GameSerializer(serializers.ModelSerializer):
     developer = GeneralUserSerializer(many=False, read_only=True)
+
+    def __init__(self, *args, **kwargs):
+        super(serializers.ModelSerializer, self).__init__(*args, **kwargs)
+        print("__init__ called")
+        if 'context' in kwargs:
+            if 'request' in kwargs['context']:
+                excluding = kwargs['context']['request'].query_params.getlist('excluding', [])
+                if excluding:
+                    if ',' in excluding[0]:
+                        excluding = excluding[0].split(',')
+
+                    excluding = set(excluding)
+                    for f in excluding:
+                        self.fields.pop(f)
+
     class Meta:
         model = Game
         fields = ['title', 'developer', 'description', 'dateOfUpload', 'averageRating', 'category', 'price', 'minimumAge', 'purchases']
 
-
-class HighscoreSerializer(serializers.HyperlinkedModelSerializer):
+class HighscoreSerializer(serializers.ModelSerializer):
     gameId = serializers.SlugRelatedField(many=False, read_only=True, slug_field='pk')
     class Meta:
         model = PlayersGames
